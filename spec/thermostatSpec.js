a98af9ad5928d0422c0a4aed659ef09eabcc5f05
Amina Jusic <amina_jusic@yahoo.com>
2019-03-07 16:42:01 +0000

    Passing test for powersave mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,32 +1,48 @@
+
 'use strict';
 
-describe('Thermostat', function() {
+ describe('Thermostat', function() {
+    var thermostat;
+    var i;
+    it('starts at 20 degrees', function(){
+        thermostat = new Thermostat();
+        expect(thermostat.temperature).toEqual(20);
+    });
 
-  var thermostat;
+    it('it can increase the temerature with an up function', function(){
+        thermostat = new Thermostat()
+        thermostat.up()
+        expect(thermostat.temperature).toEqual(21);
+    });
 
-  it('starts at 20 degrees', function() {
-    thermostat = new Thermostat();
-    expect(thermostat.temperature).toEqual(20);
-  });
+    it('it can decrease the temerature with a down function', function(){
+        thermostat = new Thermostat()
+        thermostat.down()
+        expect(thermostat.temperature).toEqual(19);
+    });
 
-  it('it can increase the temperature with an up function', function() {
-    thermostat = new Thermostat()
-    thermostat.up()
-    expect(thermostat.temperature).toEqual(21);
-  });
+    it('can not decrease the temerature below 10', function(){
+        thermostat = new Thermostat()
+        for (i = 0; i<=11; i++) {
+            thermostat.down()
+        }
+        expect(thermostat.temperature).toEqual(10);
+    });
 
-  it('can decrease the temperature with a down function', function() {
-    thermostat = new Thermostat()
-    thermostat.down()
-    expect(thermostat.temperature).toEqual(19);
-  });
+    it('can not increase the temperature above 25 if power saving mode is on', function(){
+        thermostat = new Thermostat()
+        thermostat.powersave('on')
+        for (i = 0; i<=6; i++) {
+            thermostat.up()
+        }
+        expect(thermostat.temperature).toEqual(25);
+    });
 
-  it('has a minimum of 10 degrees', function() {
-    for (var i = 0; i< 11; i++) {
-      thermostat.down()
-    }
-    expect(thermostat.temperature).toEqual(10);
-  });
+
+
+
+
+
 
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,21 +1,30 @@
-'use termostat';
-
 function Thermostat() {
-     this.temperature = 20;
-     this.minimumTemperature = 10;
+    this.temperature = 20;
+    this.minimumTemperature = 10
+    this.powersavemode = true
 
-  };
+};
 
-  Thermostat.prototype.up = function() {
-     this.temperature +=1;
-     console.log(this.temperature)
- }
+Thermostat.prototype.up = function() {
+    if (this.powersavemode === true && this.temperature < 25) {
+    this.temperature +=1 }
+    else {return this.temperature}
+    console.log(this.temperature)
+}
 
- Thermostat.prototype.down = function() {
-   if (this.temperature > this.minimumTemperature) {
-     this.temperature--
-   } else {
-     return this.temperature
-   }
-   console.log(this.temperature)
- }
+Thermostat.prototype.down = function() {
+    if (this.temperature > this.minimumTemperature) {
+    this.temperature -=1 } else
+    { return this.temperature }
+    console.log(this.temperature)
+}
+
+Thermostat.prototype.powersave = function(mode) {
+    if (mode === 'on') {
+        this.powersavemode = true
+        if (this.temperature > 25) { this.temperature = 25}
+    }
+    else if (mode === 'off') {
+        this.powersavemode = false
+    }
+}
