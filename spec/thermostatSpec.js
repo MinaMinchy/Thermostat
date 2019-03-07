806cf3c66a66e5ed2ebc1f2b2f5e85cc1d679b23
Amina Jusic <amina_jusic@yahoo.com>
2019-03-07 15:58:30 +0000

    Thermostat starts at 20 and increase by 1

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,12 +3,17 @@
 describe('Thermostat', function() {
 
   var thermostat;
-
-  beforeEach(function() {
+  
+  it('starts at 20 degrees', function() {
     thermostat = new Thermostat();
+    expect(thermostat.temperature).toEqual(20);
   });
 
-  it()
+  it('it can increase the temperature with an up function', function() {
+    thermostat = new Thermostat()
+    thermostat.up()
+    expect(thermostat.temperature).toEqual(21);
+  });
 
 
-}
+});
---
+++ src/thermostat.js
@@ -0,0 +1,11 @@
+'use termostat';
+
+function Thermostat() {
+     this.temperature = 20;
+
+  };
+
+  Thermostat.prototype.up = function() {
+     this.temperature++
+     console.log(this.temperature)
+ }
